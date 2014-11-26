2c950ed6be8b7e446e4d7f1cebf2a79bb5001a92
Mihai Dolha <mihai@playgen.com>
2014-11-26 19:44:29 +0000

    Extensively comment the shit out of the left defender

--- Robocup_Team/src/DefenderManagerLeft.java
+++ Robocup_Team/src/DefenderManagerLeft.java
@@ -65,35 +65,42 @@ public class DefenderManagerLeft implements ControllerPlayer {
 
 	/** {@inheritDoc} */
 	@Override
-	public void postInfo() {
-		if(distBall < 20) {
-			if(distBall < 0.7) {
-				if(canSeeCenter) {						
-					getPlayer().kick(100, dirCenter);
-				} else if(canSeeFlagLeft && !canSeeCenter) {					
-					getPlayer().kick(100, 90);
-				} else if(canSeeFlagRight && !canSeeCenter) {
-					getPlayer().kick(100, -90);
-				} else {
-					getPlayer().kick(100, 180);
-				}
-			} else {					
-				getPlayer().turn(dirBall);
-				getPlayer().dash(randomDashValueVeryFast());					
-			}
-		} else {
-			if(canSeeCenter && distCenter < 55){
-				if(canSeePenaltyLeft && !canSeeBall) {
-					getPlayer().turn(dirPenaltyLeft);
-					getPlayer().dash(randomDashValueVeryFast());
-				} else if(distPenaltyLeft<1){
-					getPlayer().turn(90);						
-				}
-			} else if(canSeePenaltyLeft){										
-				getPlayer().dash(randomDashValueVeryFast());
-				getPlayer().turn(dirPenaltyLeft);
-			} else if(distPenaltyLeft<1){
-				getPlayer().turn(30);						
+	public void postInfo() {									//////////////////////////////////////////////////////////////////////////////////////////////////////////
+		if(distBall < 20) { 									//if the ball is within a range of 20 from the agent (close)
+			if(distBall < 0.7) { 								//|		if the ball is within a range of 0.7 from the agent (extremely close)			
+				if(canSeeCenter) { 								//|		|	if the agent can see the center of the field					
+					getPlayer().kick(100, dirCenter); 			//|		|	|	then the agent kicks
+				} else if(canSeeFlagLeft && !canSeeCenter) {	//|		|	if the agent can't see the center of the field but can see the left touchline of the field				
+					getPlayer().kick(100, 90);					//|		|	|	then kick the ball 90 degrees clockwise from where the agent is currently facing
+				} else if(canSeeFlagRight && !canSeeCenter) {	//|		|	if the agent can't see the center of the field but can see the right touchline of the field
+					getPlayer().kick(100, -90);					//|		|	|	then kick the ball 90 degrees counterclockwise from where the agent is currently facing
+				} else {										//|		|	if the agent can't see the center of the field and neither of the touchlines
+																//|		|	this means he must be facing towards his own goal
+					getPlayer().kick(100, 180);					//|		|	|	in which case he kicks the ball 180 degrees clockwise from where he is facing
+																//|		|	in other words he shoots it towards the opposite of his own goal, being the center of the field 
+				}												//|		|
+			} else {											//|		if the ball is not within a range of 0.7 from the agent (not extremely close)
+				getPlayer().turn(dirBall);						//|		|	then turn in the direction of the ball
+				getPlayer().dash(randomDashValueVeryFast());	//|		|	and dash very fast towards the ball
+			}													//|		|
+		} else {												//if the ball is not within a range of 20 from the agent (not close)
+			if(canSeeCenter && distCenter < 55){				//|		if the agent can see the the center of the field and the distance to it is less than 55
+				if(canSeePenaltyLeft && !canSeeBall) {			//|		|	if the agent can also see the left corner of his team's penalty box but can't see the ball
+																//|		|	(meaning he is behind the penalty box, closer to the field end)
+					getPlayer().turn(dirPenaltyLeft);			//|		|	|	then turn towards the left corner of his team's penalty box
+					getPlayer().dash(randomDashValueVeryFast());//|		|	|	and dash very fast towards it
+				} else if(distPenaltyLeft<1){					//|		|	if the agent is in range 1 to the left corner of his team's penalty box
+					getPlayer().turn(90);						//|		|	|	then turn the agent 90 degrees clockwise from where he is currently facing
+				}												//|		|	|
+			} else if(canSeePenaltyLeft){						//|		if the agent can see the left corner of his own team's penalty box (but not the center of the field)
+																//|		(also this is assuming the agent is at a greater distance than 55 from the center of the field)
+				getPlayer().turn(dirPenaltyLeft);				//|		|	then turn towards the left corner of the agent's own team's penalty box
+				getPlayer().dash(randomDashValueVeryFast());	//|		|	and dash very fast towards it
+			} else if(distPenaltyLeft<1){						//|		if the agent is closer than 1 to the left corner of his own team's penalty box (assuming he can not
+																//|		see the center of the field, he is further than 55 away from it and can't see the penalty box corner)
+				getPlayer().turn(30);							//|		|	then just turn 30 degrees clockwise from where the agent is currently facing
+																//|		|	(this produces a sort of lookout for when he can't see the ball by constantly rotating him so he
+																//|		|	is always aware of his surroundings)
 			}
 		}			
 	}
@@ -118,6 +125,7 @@ public class DefenderManagerLeft implements ControllerPlayer {
 	/** {@inheritDoc} */
 	@Override
 	public void infoSeeBall(double distance, double direction, double distChange, double dirChange, double bodyFacingDirection, double headFacingDirection) {
+		//here we are saving data for when the agent sees the ball
 		distBall = distance;
 		dirBall = direction;
 		canSeeBall = true;
@@ -175,12 +183,14 @@ public class DefenderManagerLeft implements ControllerPlayer {
 	/** {@inheritDoc} */
 	@Override
 	public void infoSeeFlagRight(Flag flag, double distance, double direction, double distChange, double dirChange, double bodyFacingDirection, double headFacingDirection) {
+		//here we save data for when the agent can see the right touchline of the field
 		canSeeFlagRight = true;
 	}
 
 	/** {@inheritDoc} */
 	@Override
 	public void infoSeeFlagLeft(Flag flag, double distance, double direction, double distChange, double dirChange, double bodyFacingDirection, double headFacingDirection) {
+		//here we save data for when the agent can see the left touchline of the field
 		canSeeFlagLeft = true;
 	}
 
@@ -197,6 +207,7 @@ public class DefenderManagerLeft implements ControllerPlayer {
 	/** {@inheritDoc} */
 	@Override
 	public void infoSeeFlagCenter(Flag flag, double distance, double direction, double distChange, double dirChange, double bodyFacingDirection, double headFacingDirection) {
+		//here we are saving data for when the agent can see the center of the field
 		if(flag.compareTo(Flag.CENTER) ==0){
 			canSeeCenter = true;
 			dirCenter = direction;
@@ -217,6 +228,7 @@ public class DefenderManagerLeft implements ControllerPlayer {
 	/** {@inheritDoc} */
 	@Override
 	public void infoSeeFlagPenaltyOwn(Flag flag, double distance, double direction, double distChange, double dirChange, double bodyFacingDirection, double headFacingDirection) {
+		//here we are saving data for when the agent sees the left corner of his own team's penalty box
 		if(flag.compareTo(Flag.LEFT)==0){
 			canSeePenaltyLeft = true;
 			dirPenaltyLeft = direction;
