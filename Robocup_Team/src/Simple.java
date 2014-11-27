dc027667b1dd20ce931a2ff40b5c3269aac4d6e2
MikeD <akagetsu@gmail.com>
2014-11-27 16:39:04 +0000

    Do the overall comment ting

--- Robocup_Team/src/Simple.java
+++ Robocup_Team/src/Simple.java
@@ -20,6 +20,42 @@ import org.apache.log4j.Logger;
 import java.util.HashMap;
 import java.util.Random;
 
+// ROBOCUP PEAS:
+
+// Performance: play a game of 2D robocyp footbal simulator following the rules and score more goals than the enemy team
+// Environment: Known environment
+// Actuators: Legs to dash and kick the ball, body to turn, hands to pick up the ball (goalie only)
+// Sensors: Eyes to view the 90 degree FOV, ears to hear commands by other players or the coach
+
+// ROBOCUP TASK ENVIRONMENT:
+
+// Partially Observable:
+//     The player can only see what is in range of the 90 degree viewing sensor, everything else
+//     the player assumes it is either still there in the same direction and distance it saw it last
+//     or he assumes doesn't even exist anymore.
+// Multi-Agent:
+//     There are multiple thinking entities in the game at once and each may have similar or 
+//     different state machines and behaviour
+// Deterministic:
+//     It is guaranteed that kicking a ball in a direction will send it to that direction.
+//     The agent will do anything you program it to do.
+// Sequential:
+//     At different states and at different times or distances, the agent does something different.
+//     For instance, episodic environments would assume that each player does exactly the same thing
+//     in every scenario regardless of position or state
+// Dynamic:
+//     While the player deliberates, the game goes on (ie, if the player is finding the furthest own
+//     player then the game still goes on, he can get tackled while he is thinking)
+// Continuous:
+//     The player can do a multitude of actions at once, such as kick and run at the same time and
+//     look and run
+// Partially Known Environment:
+//     Every player knows the rules of the game, but unless you constantly tell the player the distance
+//     between itself and things it hasn't seen for a while, it won't know
+
+// Agent Type: Simple Reflex Agent
+//     because it reacts to the current state of it's environment without taking into account what happened before
+
 /**
  * A simple controller. It implements the following simple behaviour. If the
  * client sees nothing (it might be out of the field) it turns 180 degree. If
