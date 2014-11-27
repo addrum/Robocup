69fb03316dee83fe750aaaeb5482863c78c0ecd0
Adam Short <adamshort1994@gmail.com>
2014-11-27 15:11:43 +0000

    merge with adam

---
+++ Robocup_Team/src/SimplySimpleTeam.java
@@ -0,0 +1,51 @@
+
+//~--- non-JDK imports --------------------------------------------------------
+
+import com.github.robocup_atan.atan.model.AbstractTeam;
+import com.github.robocup_atan.atan.model.ControllerCoach;
+import com.github.robocup_atan.atan.model.ControllerPlayer;
+
+/**
+ * A class to setup a Simple Silly AbstractTeam.
+ *
+ * @author Atan
+ */
+public class SimplySimpleTeam extends AbstractTeam {
+
+    /**
+     * Constructs a new simple silly team.
+     *
+     * @param name The team name.
+     * @param port The port to connect to SServer.
+     * @param hostname The SServer hostname.
+     * @param hasCoach True if connecting a coach.
+     */
+    public SimplySimpleTeam(String name, int port, String hostname, boolean hasCoach) {
+        super(name, port, hostname, hasCoach);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * The first controller of the team is the goalie and the others are players (11 is for the captain).
+     */
+    @Override
+    public ControllerPlayer getNewControllerPlayer(int number) {
+        if (number == 0) 
+            return new GoalieManager();
+        else if (number == 1)
+        	return new AttackerManager();
+        else 
+            return new Simple();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * Generates a new coach.
+     */
+    @Override
+    public ControllerCoach getNewControllerCoach() {
+        return new Coach();
+    }
+}
